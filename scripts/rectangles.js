1140fe59a869e15c553264f6e035fd00d839efe1
Storms2000 <stoneztao@gmail.com>
2018-07-20 13:44:19 -0400

    Initial

--- scripts/rectangles.js
+++ scripts/rectangles.js
@@ -1,9 +1,24 @@
 var cWidth = window.innerWidth;
 var cHeight = window.innerHeight;
 var colors2 = ["#fde74c","#ffbb32",  "#fa7921", "#f93416","#ff1654","#f3ffbd","#b2dbbf","#247ba0","#06d6a0"];
-var rectangleColors = [];
-$(document).on("ready",function(){
+var rectangleColors = ["#ffffff"];
 
+var single = true;
+$(document).on("ready",function(){
+  $("#reset").on("click",function(){
+    //reset colors
+    rectangleColors = ["#ffffff"];
+    
+    //Reset rectangles
+    cWidth = window.innerWidth;
+    cHeight = window.innerHeight;
+    rectangles = [[0,0,cWidth,cHeight]];
+
+  })
+  
+  $("#generate").on("click",function(){
+    generateRectangles(rectangles,60);
+  })
   $(document).mousemove(function(e){
 
   });
@@ -19,7 +34,10 @@ function setup(){
 }
 
 function draw(){
-  for (var i =0; i<rectangles.length;i++){
+  if (single != true){
+    background(255,255,255);
+  }
+  for (var i = rectangles.length-1; i>=0;i--){
     if (pointInRect(mouseX,mouseY,rectangles[i])){
       fill(rectangleColors[i]);
       rect(rectangles[i][0],rectangles[i][1],rectangles[i][2],rectangles[i][3])
@@ -28,8 +46,8 @@ function draw(){
 }
 var rectangles = [[0,0,cWidth,cHeight]];
 var count = 0;
-function generateRectangles(rectArr){
-  if (count < 10){
+function generateRectangles(rectArr, max){
+  if (count < max){
     
     var randomIndex = round(random(0,1) * (rectArr.length - 1));
     
@@ -40,40 +58,47 @@ function generateRectangles(rectArr){
     var xv;
     var yv;
     //console.log(rectArr,"before")
-    if (random(0,1) < 0.5){
+    if (random(0,1) <= 0.5){
       //split left right
-      yv = random(0,1) * (chosenRect[3]);
+      yv = round (random(0,1) * (chosenRect[3]));
       //if too small, redo
-      if (yv < 100){
-        return generateRectangles(rectArr);
+      if (yv < 50 || chosenRect[3] - yv < 50){
+        max--;
+        return generateRectangles(rectArr, max);
       }
     }
     else {
-      xv = random (0,1) * (chosenRect[3]);
-      if (xv < 100){
-        return generateRectangles(rectArr);
+      xv = round (random (0,1) * (chosenRect[2]));
+      if (xv < 50 || chosenRect[2] - xv < 50){
+        max--;
+        return generateRectangles(rectArr, max);
       }
     }
-    rectArr.splice(randomIndex,1);
+    //console.log(rectArr.slice(0));
+    
     if (yv){
       //left to right split
       //Retains width, and height yv, and chosenRect[3]-yv;
       //New pos at original and originalx, originaly+yv
       rectArr.push([chosenRect[0],chosenRect[1],chosenRect[2], yv ])
-      rectArr.push([chosenRect[0],chosenRect[1]+yv,chosenRect[2], chosenRect[3]-yv]);
+      rectArr.push([chosenRect[0],chosenRect[1]+yv,chosenRect[2], abs(chosenRect[3]-yv)]);
     }
-    else {
+    else if (xv){
       //up down split
       //Retains height, and width xv, and chosenRect[2]-xv;
       //New pos at original and originaly, originalx+xv
       rectArr.push([chosenRect[0],chosenRect[1], xv, chosenRect[3]])
-      rectArr.push([chosenRect[0]+xv,chosenRect[1], chosenRect[2] - xv, chosenRect[3]])
+      rectArr.push([chosenRect[0]+xv,chosenRect[1], abs(chosenRect[2] - xv), chosenRect[3]])
       
     }
-    
-    count++;
+    else {
+      return generateRectangles(rectArr,max);
+    }
+    rectArr.splice(randomIndex,1);
+    console.log(rectArr.slice(0));
+    max--;
     //console.log(rectArr,"after")
-    return generateRectangles(rectArr);
+    return generateRectangles(rectArr,max);
   }
   else {
     displayRectangles(rectArr)
@@ -88,11 +113,11 @@ function generateRectangles(rectArr){
 }
 
 function displayRectangles(rectArr){
-  for (var i=0;i<rectArr.length;i++){
+  for (var i=rectArr.length-1; i>=0;i--){
     var randomIndex = round(random(0,1) * (colors2.length -1));
-    fill(colors2[randomIndex]);
+    //fill(colors2[randomIndex]);
     rectangleColors.push(colors2[randomIndex]);
-    rect(rectArr[i][0],rectArr[i][1],rectArr[i][2],rectArr[i][3])
+    //rect(rectArr[i][0],rectArr[i][1],rectArr[i][2],rectArr[i][3])
   }
 }
 
